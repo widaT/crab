bc1e8bae1064d6d231152c4ab48437df552830f8
wida <wida@foxmail.com>
2022-01-06 17:14:11 +0800

    添加js client demo

---
+++ jsclient/main.js
@@ -0,0 +1,18 @@
+// Create WebSocket connection.
+const socket = new WebSocket('ws://localhost:8888/?token=abcd');
+
+// Connection opened
+socket.addEventListener('open', function (event) {
+    //认证
+    a ={type:0,payload:"no578999"}
+    console.log(JSON.stringify(a))
+    socket.send(JSON.stringify(a));
+});
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+    const e = document.getElementById("messages");
+    e.innerHTML = e.innerHTML + 'Message from server '+ event.data +"<br/>";
+
+    console.log('Message from server ', event.data)
+});
